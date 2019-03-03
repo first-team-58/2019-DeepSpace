0b6e34af9950aa70c1eb3d5aa82fc97fc96bf7c7
Jack Manning <jmanning@automec.com>
2019-03-03 11:30:24 -0500

    togglePID now toggles pid and manual modes

--- src/main/java/frc/robot/commands/TogglePID.java
+++ src/main/java/frc/robot/commands/TogglePID.java
@@ -4,23 +4,23 @@ import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
 public class TogglePID extends Command{
-	private static boolean toggle = true;
+	private static boolean toggle = false;
 	
 	public TogglePID() {
 		toggle = !toggle;
 	}
 	
 	protected void execute() {
-		Robot.manualMode = true;
-		Robot.m_Wrist.positionAchieved = true;
-		Robot.m_Elevator.positionAchieved = true;
-		Robot.m_Shoulder.positionAchieved = true;
+		Robot.manualMode = toggle;
+		Robot.m_Wrist.positionAchieved = toggle;
+		Robot.m_Elevator.positionAchieved = toggle;
+		Robot.m_Shoulder.positionAchieved = toggle;
 	}
 	
 	@Override
 	protected boolean isFinished() {
 		// TODO Auto-generated method stub
-		return toggle;
+		return true;
 	}
 
 }
